b9bc44276e7f31bcadca846cf7ebe10cc8c84f6d
IkedaLab-Daniel <98075061+IkedaLab-Daniel@users.noreply.github.com>
2024-08-26 23:21:58 +0800

    Update Footer.jsx

--- src/componets-second/Footer.jsx
+++ src/componets-second/Footer.jsx
@@ -1,3 +1,5 @@
+import reactPNG from '../assets/react.png';
+
 function Footer(){
     return(
         <footer>
@@ -12,7 +14,7 @@ function Footer(){
                 <a className="emit-mobile" target="_blank" href="https://www.facebook.com/markdaniel.callejas.9">Facebook</a><span>|</span>
                 <a className="emit-mobile" target="_blank" href="https://www.instagram.com/danieldaniel.dit/">Instagram</a><span>|</span>
                 <a className="emit-mobile" target="_blank" href="https://www.benevolent-sunburst-33739d.netlify.app">Portfolio</a>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png" alt="" />
+                <img src={reactPNG} alt="" />
                 <p>React JS</p>
             </div>
         </footer>
