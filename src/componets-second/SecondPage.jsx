c8905297b5cecbdb46647818064a7cc65da26995
IkedaLab-Daniel <98075061+IkedaLab-Daniel@users.noreply.github.com>
2024-09-07 20:27:22 +0800

    Update SecondPage.jsx

--- src/componets-second/SecondPage.jsx
+++ src/componets-second/SecondPage.jsx
@@ -1,133 +1,176 @@
-import React, {useState, useEffect} from 'react';
-import Kagura from '../assets/kagura.webp'
+import React, { useState, useEffect } from 'react';
+import Kagura from '../assets/kagura.webp';
 
-function SecondPage(){
 
-    const [result, setResult] = useState("Please, input the details needed ~");
-    const [currentWR, setCurrentWR] = useState();
-    const [totalMatches, setTotalMatches] = useState();
-    const [targetWR, setTargetWR] = useState();
-    const [currentWins, setCurrentWins] = useState();
-    const [winNeeded, setWinNeeded] = useState();
-    const [buttonText, setButtonText] = useState('Ask Kagura!');
-    const [count, setCount] = useState(0);
+function SecondPage() {
+  const [state, setState] = useState({
+    result: "Please, input the details needed ~",
+    currentWR: '',
+    totalMatches: '',
+    targetWR: '',
+    currentWins: 0,
+    winNeeded: 0,
+    buttonText: '...',
+    count: 0,
+    wrPointer: 0,
+    floatTargetWR: 0,
+    finalResult: 0,
+    buttonDisable: true,
+    buttonClass: "buttonDisable",
+  });
 
-    /* v2 */
+    // Function to handle image download
+    const handleDownload = () => {
+      const link = document.createElement('a');
+      link.href = Kagura;  // URL of the image to download
+      link.download = 'kagura.webp';  // Name of the downloaded file
+      document.body.appendChild(link);
+      link.click();  // Trigger download
+      document.body.removeChild(link);  // Clean up the link element
+    };
+  
 
-    const [wrPointer, setWrPointer] = useState(); 
-    const [floatTargetWR, setFloatTargetWR] = useState();
-    const [finalResult, setFinalResult] = useState();
+  function changeCurrentWR(event) {
+    setState(prevState => ({
+      ...prevState,
+      currentWR: event.target.value,
+      count: 0,
+    }));
+  }
 
-    function changeCurrentWR(event){
-        setCurrentWR(event.target.value);
-        setCount(0)
-        setButtonText('Ask Kagura');
+  function changeTotalMatches(event) {
+    setState(prevState => ({
+      ...prevState,
+      totalMatches: event.target.value,
+      count: 0,
+    }));
+  }
 
-        
+  function changeTargetWR(event) {
+    setState(prevState => ({
+      ...prevState,
+      targetWR: event.target.value,
+      count: 0,
+    }));
+  }
 
+  useEffect(() => {
+    const wrPointer = state.currentWR / 100;
+    const floatTargetWR = state.targetWR / 100;
+    const currentWins = wrPointer * state.totalMatches;
+
+    const winNeeded = (floatTargetWR * state.totalMatches - currentWins) / (1 - floatTargetWR);
+    const finalResult = Math.ceil(winNeeded);
+
+    setState(prevState => ({
+      ...prevState,
+      wrPointer,
+      floatTargetWR,
+      currentWins,
+      winNeeded,
+      finalResult,
+    }));
+
+    if (state.currentWR === 'yz') {
+      setState(prevState => ({
+        ...prevState,
+        result: 'Savage ni Brix'
+      }));
     }
 
-    function changeTotalMatches(event){
-        setTotalMatches(event.target.value);
-        setCount(0)
-        setButtonText('Ask Kagura');
+    if (state.currentWR == "" || state.totalMatches == "" || state.targetWR == ""){
+      setState(prevState => ({
+        ...prevState,
+        buttonText: "...",
+        buttonDisable: true,
+        buttonClass: "buttonDisable",
+      }))
+    } else{
+      setState(prevState => ({
+        ...prevState,
+        buttonText: "Ask Kagura!",
+        buttonDisable: false,
+        buttonClass: "buttonEnable",
+      }))
     }
 
-    function changeTargetWR(event){
-        setTargetWR(event.target.value);
-        setCount(0)
-        setButtonText('Ask Kagura');
+    if (state.targetWR > 100){
+      setState(prevState => ({
+        ...prevState,
+        result: "You can't have more than 100% win rate!"
+      }))
+    }
+  }, [state.currentWR, state.totalMatches, state.targetWR]);
 
-        
-    }  
+  function calculate() {
+    setState(prevState => {
+      const newCount = prevState.count + 1;
 
-    useEffect(() => {
+      let newResult;
+      if (prevState.finalResult < 0) {
+        newResult = `You want to lower your winrate? If so, you need to lose ${(prevState.finalResult - prevState.finalResult * 2)} matches to get ${prevState.targetWR}% winrate!`;
+      } else {
+        newResult = `Hmm, in order to get ${prevState.targetWR}% win rate, you have to win ${prevState.finalResult} more matches!`;
+      }
 
-        setFloatTargetWR (ftwr => targetWR / 100); 
-        setCurrentWins(cw =>wrPointer * totalMatches);
-        setWrPointer(wrp => currentWR/100); 
-        /*
-        console.log('********  Variables  ***************');
-        console.log("Target WR: ", targetWR);
-        console.log("Target WR (FLOAT): ", floatTargetWR);
-        console.log("Total Matches: ", totalMatches);
-        console.log("Current Wins: ", currentWins);
-        console.log("Win Needed: ", winNeeded);
-        console.log("Current WR: ", currentWR);
-        console.log("Current WR (float): ", wrPointer);
-        console.log('*********************************');
-        
-        */
-        
+      let newState = {
+        ...prevState,
+        count: newCount,
+        result: newResult,
+      };
 
-        setWinNeeded(wn =>(floatTargetWR*totalMatches-currentWins)/(1-floatTargetWR));
-        setFinalResult(sf => Math.ceil(winNeeded));
-        /* 
-            console.log("----------------- Win Needed: ", winNeeded);
-        */
-        
-        
-        /* Easter Egg */
-        if (currentWR == 'yz'){
-            setResult('Savage ni Brix');
-        }
+      if (newCount > 2) {
+        newState = {
+          ...newState,
+          currentWR: '',
+          totalMatches: '',
+          targetWR: '',
+          result: '...',
+        };
+      }
+
+      return newState;
     });
+  }
 
-    function calculate(){
-            setCount(c => c + 1);
+  return (
+    <div id="second-page">
+      <div className="main-container">
+        <div className="data-part">
+          <label htmlFor="CurrentWinRate">Curent Win Rate (%)</label><br />
+          <input 
+            type="number"
+            placeholder=""
+            onChange={changeCurrentWR}
+            value={state.currentWR}
+            name="CurrentWinRate" />
 
-            if (finalResult < 0){
-                setResult(r => `You want to lower your winrate? If so, you need to loss ${(finalResult - finalResult * 2)} matches to get ${targetWR}% winrate!`);
-            } else{
-                setResult(r => `Hmm, in order to get ${targetWR}% win rate, you have to win ${finalResult} more matches!`);
-            }
-            
-            if (count > 2){
-                setCurrentWR('');
-                setTotalMatches('');
-                setTargetWR('');
-                setResult('...');
-                setButtonText('Ask Kagura')
-            }
+          <label htmlFor="TotalMatches">Total Matches</label><br />
+          <input 
+            type="number"
+            placeholder=""
+            onChange={changeTotalMatches}
+            value={state.totalMatches}
+            name="TotalMatches" />
 
-            
-    }
+          <label htmlFor="TargetWinRate">Target WinRate (%)</label><br />
+          <input 
+            type="number"
+            placeholder=" "
+            onChange={changeTargetWR}
+            value={state.targetWR}
+            name="TargetWinRate" />
 
-    return(
-        <div id="second-page">
-            <div className="main-container">
-                <div className="data-part">
-                    <label htmlFor="CurrentWinRate">Curent Win Rate (%)</label><br></br>
-                    <input typeof="numberInput"
-                    placeholder="" 
-                    onChange={changeCurrentWR}
-                    value={currentWR}
-                    name="CurrentWinRate"/>
-
-                    <label htmlFor="TotalMatches">Total Matches</label><br></br>
-                    <input typeof="numberInput" 
-                    placeholder="" 
-                    onChange={changeTotalMatches}
-                    value={totalMatches}
-                    name="TotalMatches"/>
-
-                    <label htmlFor="TargetWinRate">Target WinRate (%)</label><br></br>
-                    <input typeof="numberInput" 
-                    placeholder=" " 
-                    onChange={changeTargetWR}
-                    value={targetWR}
-                    name="TargetWinRate"/>
-
-                    <button onClick={calculate}>{buttonText}</button>
-                </div>
-                <div className="result-part">
-                    <h1></h1>
-                    <p>{result}</p>
-                    <img src={Kagura} alt="" />
-                </div>
-            </div>
+          <button className={state.buttonClass} onClick={calculate} disabled={state.buttonDisable}>{state.buttonText}</button>
         </div>
-    )
+        <div className="result-part">
+          <h1></h1>
+          <p>{state.result}</p>
+          <img src={Kagura} onClick={handleDownload} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default SecondPage
+export default SecondPage;
